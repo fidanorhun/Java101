f4a572785fe18ec424b842bdc87774d4a267e574
FidanOrhun <61371851+fidanorhun@users.noreply.github.com>
2022-03-07 23:55:57 +0300

    KDV Tutarı Hesaplayan Program

--- KDVOranı.java
+++ KDVOranı.java
@@ -1,4 +1,23 @@
 
+import java.util.Scanner;
+
+public class KDVOranı {
+    public static void main(String[] args) {
 
 
+        double Tutar, KdvOrani = 0.18;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Tutar giriniz:");
+        Tutar=inp.nextInt();
+
+        KdvOrani=(0<Tutar && Tutar<1000? 0.18:0.08 );
+
+        Tutar=Tutar+(Tutar*KdvOrani);
+
+        System.out.println("Toplam değer:"+Tutar);
+
+
+    }
 }
